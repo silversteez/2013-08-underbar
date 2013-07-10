90929797d283f0c4bd52d11830a165398bc82336
matt silverstein <welcomematt@gmail.com>
2013-07-10 13:03:51 -0700

    Got indexOf working

--- src/underbar.js
+++ src/underbar.js
@@ -53,10 +53,17 @@ var _ = { };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
-  _.indexOf = function(array, target){
+  _.indexOf = function(array, target) {
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
+    for ( var i = 0, len = array.length; i < len; i++) {
+      console.log(array[i]);
+      if (array[i] === target) {
+        return i;
+      }
+    }
+    return -1;
   };
 
   // Return all elements of an array that pass a truth test.
